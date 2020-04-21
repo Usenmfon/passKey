6f38a636b7c1cc778b0bf3763fcd7f2858f79b85
Usenmfon <usenmfonuko@gmail.com>
2020-04-21 15:18:41 +0100

    password code

---
+++ passKey/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace passKey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==Simple code to authenticate user==\n");
+            string password = "";
+            
+            while(password != "free")
+            {
+                Console.Write("***Welcome to Ozum\n\n"+
+                        "***Please enter password: ");
+                password = Console.ReadLine();
+
+            }
+            Console.WriteLine("\nACCESS GRANTED+++ENJOY YOUR STAY");
+        }
+    }
+}
